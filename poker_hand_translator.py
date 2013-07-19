05d451e23de21495887c405e7ce754a6a65a9c65
Diego Guemes <diegogp1988@gmail.com>
2013-07-19 11:33:36 +0200

    Two pair test passing

--- poker_hand_translator.py
+++ poker_hand_translator.py
@@ -3,6 +3,8 @@ from poker_hands import *
 class PokerHandTranslator:
 
     def translate(self, hand):
+        if self.__is_two_pair(hand):
+            return TWO_PAIR
         if self.__is_pair(hand):
             return PAIR
         return HIGH_CARD
@@ -10,3 +12,7 @@ class PokerHandTranslator:
     def __is_pair(self, hand):
         values = [card[:-1] for card in hand]
         return len(set(values)) == len(hand) - 1
+
+    def __is_two_pair(self, hand):
+        values = [card[:-1] for card in hand]
+        return len(set(values)) == len(hand) - 2
