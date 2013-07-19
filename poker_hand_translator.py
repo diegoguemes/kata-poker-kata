c8032f4174b0b4e39ef009565d1fb1403445ae42
Diego Guemes <diegogp1988@gmail.com>
2013-07-19 11:48:09 +0200

    Three of a kind test passing

--- poker_hand_translator.py
+++ poker_hand_translator.py
@@ -3,6 +3,8 @@ from poker_hands import *
 class PokerHandTranslator:
 
     def translate(self, hand):
+        if self.__is_three_of_a_kind(hand):
+            return THREE_OF_A_KIND
         if self.__is_two_pair(hand):
             return TWO_PAIR
         if self.__is_pair(hand):
@@ -14,6 +16,15 @@ class PokerHandTranslator:
 
     def __is_two_pair(self, hand):
         return self.__distinct_values_count(hand) == len(hand) - 2
+
+    def __is_three_of_a_kind(self, hand):
+        max = 0
+        values = [card[:-1] for card in hand]
+        for value in set(values):
+            count = values.count(value)
+            if count > max:
+                max = count
+        return max == 3
 
     def __distinct_values_count(self, hand):
         values = [card[:-1] for card in hand]
