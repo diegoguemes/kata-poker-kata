9c134bd5254faeaec0794ebd5e20a7549e54f120
Diego Guemes <diegogp1988@gmail.com>
2013-07-19 11:31:34 +0200

    Two pair test failing

--- poker_hand_translator_test.py
+++ poker_hand_translator_test.py
@@ -13,6 +13,9 @@ class PokerHandTranslatorTest(unittest.TestCase):
     def test_translates_pair_hand(self):
         self.assertEqual(PAIR, self.translate(['2♦', '2♥', 'Q♥', '7♥', '6♣']))
 
+    def test_translates_two_pair_hand(self):
+        self.assertEqual(TWO_PAIR, self.translate(['K♣', 'K♠', '9♥', '9♦', 'J♥']))
+
     def translate(self, hand):
         return self.poker_hand_translator.translate(hand)
 
