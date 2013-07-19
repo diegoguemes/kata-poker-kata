adc2e4cc40acc706159c4b8f3392b4bbcdfd9d97
Diego Guemes <diegogp1988@gmail.com>
2013-07-19 11:17:27 +0200

    Pair test failing

--- poker_hand_translator.py
+++ poker_hand_translator.py
@@ -1,5 +1,6 @@
 from poker_hands import *
 
 class PokerHandTranslator:
+
     def translate(self, hand):
         return HIGH_CARD
--- poker_hand_translator_test.py
+++ poker_hand_translator_test.py
@@ -10,6 +10,9 @@ class PokerHandTranslatorTest(unittest.TestCase):
     def test_translates_high_card_hand(self):
         self.assertEqual(HIGH_CARD, self.translate(['A♥', 'K♥', 'Q♣', '10♥', '2♠']))
 
+    def test_translates_pair_hand(self):
+        self.assertEqual(PAIR, self.translate(['2♦', '2♥', 'Q♥', '7♥', '6♣']))
+
     def translate(self, hand):
         return self.poker_hand_translator.translate(hand)
 
