8d72a5a5bcd0609927933772c63cc9bf3d701f21
Diego Guemes <diegogp1988@gmail.com>
2013-07-19 11:25:22 +0200

    Pair test passing

--- poker_hand_translator.py
+++ poker_hand_translator.py
@@ -3,4 +3,7 @@ from poker_hands import *
 class PokerHandTranslator:
 
     def translate(self, hand):
+        values = set([card[:-1] for card in hand])
+        if len(values) == len(hand) - 1:
+            return PAIR
         return HIGH_CARD
