0235a737fe422d8f6c9879955da5343ca0b2362c
Diego Guemes <diegogp1988@gmail.com>
2013-07-19 17:23:08 +0200

    Fixed calls to rule methods

--- poker_hand_translator.py
+++ poker_hand_translator.py
@@ -7,21 +7,21 @@ class HandTranslator:
 
     def translate(self, hand):
         for hand_type, hand_rule in self.hand_rules:
-            if hand_rule(hand):
+            if hand_rule.is_satisfied_by(hand):
                 return hand_type
         return None
 
 def create_poker_hand_translator():
     poker_hand_rules = sorted({
-        HIGH_CARD: HighCardRule().is_satisfied_by,
-        PAIR: PairRule().is_satisfied_by,
-        TWO_PAIR: TwoPairRule().is_satisfied_by,
-        THREE_OF_A_KIND: ThreeOfAKindRule().is_satisfied_by,
-        STRAIGHT: StraightRule().is_satisfied_by,
-        FLUSH: FlushRule().is_satisfied_by,
-        FULL_HOUSE: FullHouseRule().is_satisfied_by,
-        FOUR_OF_A_KIND: FourOfAKindRule().is_satisfied_by,
-        STRAIGHT_FLUSH: StraightFlushRule().is_satisfied_by,
-        ROYAL_FLUSH: RoyalFlushRule().is_satisfied_by
+        HIGH_CARD: HighCardRule(),
+        PAIR: PairRule(),
+        TWO_PAIR: TwoPairRule(),
+        THREE_OF_A_KIND: ThreeOfAKindRule(),
+        STRAIGHT: StraightRule(),
+        FLUSH: FlushRule(),
+        FULL_HOUSE: FullHouseRule(),
+        FOUR_OF_A_KIND: FourOfAKindRule(),
+        STRAIGHT_FLUSH: StraightFlushRule(),
+        ROYAL_FLUSH: RoyalFlushRule()
     }.items(), reverse=True)
     return HandTranslator(poker_hand_rules)
