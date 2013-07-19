e8e54fd0778d0ba684c20d63bfe29aa612ea9651
Diego Guemes <diegogp1988@gmail.com>
2013-07-19 11:52:53 +0200

    Extract method max_repeated_value_count

--- poker_hand_translator.py
+++ poker_hand_translator.py
@@ -17,15 +17,18 @@ class PokerHandTranslator:
     def __is_two_pair(self, hand):
         return self.__distinct_values_count(hand) == len(hand) - 2
 
-    def __is_three_of_a_kind(self, hand):
-        max = 0
-        values = [card[:-1] for card in hand]
-        for value in set(values):
-            count = values.count(value)
-            if count > max:
-                max = count
-        return max == 3
-
     def __distinct_values_count(self, hand):
         values = [card[:-1] for card in hand]
         return len(set(values))
+
+    def __is_three_of_a_kind(self, hand):
+        return self.__max_repeated_value_count(hand) == 3
+
+    def __max_repeated_value_count(self, hand):
+        max_count = 0
+        values = [card[:-1] for card in hand]
+        for value in set(values):
+            count = values.count(value)
+            if count > max_count:
+                max_count = count
+        return max_count
